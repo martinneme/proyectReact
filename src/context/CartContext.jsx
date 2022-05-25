3e80e420e5e5d456222b9b0df31d42dc77be7c49
mneme07 <mjneme1995@gmail.com>
2022-05-25 19:26:13 -0300

    CartContext

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,7 +1,6 @@
 import { createContext } from "react";
 import { useState } from "react";
 
-
 export const cartContext = createContext();
 const Provider = cartContext.Provider;
 
@@ -9,38 +8,38 @@ export default function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
 
   const addToCart = (item, quantity) => {
-
-    if (isInCart(item.category)) {
+    if (isInCart(item.id)) {
       const newCart = cart.map((cartItem) => {
-        if (cartItem.category === item.category) {
-          cartItem.quantity+=quantity;
+        if (cartItem.id === item.id) {
+          cartItem.quantity += quantity;
         }
         return cartItem;
       });
       setCart(newCart);
     } else {
-      console.log("entro al else del addtoCArt")
+      console.log("entro al else del addtoCArt");
       setCart([...cart, { ...item, quantity: quantity }]);
     }
   };
 
-  const clearCart = () => setCart([])
+  const clearCart = () => setCart([]);
 
-
-  const removeItemCart = (category) => {
-    console.log(cart)
-    if (isInCart(category)) {
-      const newCart = cart.filter((itemCart) => itemCart.category !== category)
+  const removeItemCart = (id) => {
+    if (isInCart(id)) {
+      const newCart = cart.filter((itemCart) => itemCart.id !== id);
       setCart(newCart);
     } else {
-      console.log("no existe este objeto en el carrito")
+      console.error("no existe este objeto en el carrito");
     }
-    console.log(cart)
-  }
-
-  const isInCart = (category) => {
-  return cart.find((item) =>  item.category === category);
   };
 
-  return <Provider value={{ addToCart,removeItemCart,clearCart, cart }}>{children}</Provider>;
+  const isInCart = (id) => {
+    return cart.find((item) => item.id === id);
+  };
+
+  return (
+    <Provider value={{ addToCart, removeItemCart, clearCart, cart }}>
+      {children}
+    </Provider>
+  );
 }
