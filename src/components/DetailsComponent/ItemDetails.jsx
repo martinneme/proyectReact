3be0a85f7af4a71789fc88f972f4ae487205b10e
mneme07 <mjneme1995@gmail.com>
2022-05-25 19:21:41 -0300

    CartContext

--- src/components/DetailsComponent/ItemDetails.jsx
+++ src/components/DetailsComponent/ItemDetails.jsx
@@ -11,7 +11,7 @@ export default function ItemDetails({ productDetail }) {
 
   const nav = useNavigate();
 
-  const { addToCart } = useContext(cartContext);
+  const { addToCart} = useContext(cartContext);
 
   const [enableCount, setEnableCount] = useState(true);
   const [quantity, setQuantity] = useState(0);
@@ -21,6 +21,9 @@ export default function ItemDetails({ productDetail }) {
     setEnableCount(false);
     setQuantity(quantity);
   };
+
+
+
 
   return (
     <>
@@ -48,10 +51,11 @@ export default function ItemDetails({ productDetail }) {
                 />
               ) : (
                 <>
-                <h6>Se agregó {quantity} productos al carrito </h6>
+                <h6>Se agregó {quantity} producto/s al carrito </h6>
                 <Button variant="primary btnVolver" onClick={() => nav(`/cart`)}>
                 Finalizar Compra
-              </Button></>
+              </Button>
+              </>
               )}
             </ListGroupItem>
           </ListGroup>
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,6 +1,7 @@
 import { createContext } from "react";
 import { useState } from "react";
 
+
 export const cartContext = createContext();
 const Provider = cartContext.Provider;
 
@@ -8,27 +9,38 @@ export default function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
 
   const addToCart = (item, quantity) => {
-    if (isInCart(item.id)) {
+
+    if (isInCart(item.category)) {
       const newCart = cart.map((cartItem) => {
-        if (cartItem.id === item.id) {
-          cartItem.quantity++;
+        if (cartItem.category === item.category) {
+          cartItem.quantity+=quantity;
         }
         return cartItem;
       });
       setCart(newCart);
     } else {
+      console.log("entro al else del addtoCArt")
       setCart([...cart, { ...item, quantity: quantity }]);
     }
   };
 
-  const isInCart = (id) => {
-    cart.map((item) => {
-      if (item.id === id) {
-        return true;
-      }
-      return false;
-    });
+  const clearCart = () => setCart([])
+
+
+  const removeItemCart = (category) => {
+    console.log(cart)
+    if (isInCart(category)) {
+      const newCart = cart.filter((itemCart) => itemCart.category !== category)
+      setCart(newCart);
+    } else {
+      console.log("no existe este objeto en el carrito")
+    }
+    console.log(cart)
+  }
+
+  const isInCart = (category) => {
+  return cart.find((item) =>  item.category === category);
   };
 
-  return <Provider value={{ addToCart, cart }}>{children}</Provider>;
+  return <Provider value={{ addToCart,removeItemCart,clearCart, cart }}>{children}</Provider>;
 }
